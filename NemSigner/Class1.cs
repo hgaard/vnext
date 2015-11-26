6526b001bd82115edaec5f439fa41c00608d0104
Jakob Højgaard <jakob@hgaard.dk>
2015-11-26 20:53:50 +0100

    Initial commit

---
+++ NemSigner/Class1.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NemSigner
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+        public static string Hello()
+        {
+            return "Hello";
+        }
+    }
+}
